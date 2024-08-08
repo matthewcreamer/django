f7c493623f2537f56feb09df2c3a1e46c230c833
matthewcreamer <157339577+matthewcreamer@users.noreply.github.com>
2024-08-08 09:54:49 +0700

    update exptable

--- commands.py
+++ commands.py
@@ -5,3 +5,9 @@
 #   python3 manage.py runserver
 
 #   install with pip3
+
+
+
+# name = models.CharField(max_length=255)
+# val = models.FloatField()
+# val2 = models.DecimalField(max_digits=10, decimal_places=2)
